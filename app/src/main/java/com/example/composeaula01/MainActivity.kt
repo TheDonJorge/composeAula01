86907d08908cd92b387699e2235011b8573406db
JorgeLucas <83237763+TheDonJorge@users.noreply.github.com>
2026-06-20 18:24:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/composeaula01/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.example.composeaula01
+
+import android.R
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeaula01.ui.theme.ComposeAula01Theme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+
+@Composable
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboradingScreen(onContinuedClicked = {shouldShowOnboarding = false})
+    } else {
+        Greetings()
+    }
+
+}
+
+
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            for (name in names){
+                Greeting(name)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting(name: String){
+    var expanded = remember  {mutableStateOf(false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(Modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "Hello, ",)
+                Text(text = name,)
+            }
+            OutlinedButton(onClick = {expanded.value = !expanded.value}) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboradingScreen(
+    onContinuedClicked: () -> Unit
+) {
+
+
+    Surface() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Bem vindos ao Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinuedClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultPreview() {
+    MyApp()
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeAula01Theme() {
+        OnboradingScreen(
+            onContinuedClicked = TODO()
+        )
+    }
+}
+
+
+
+
